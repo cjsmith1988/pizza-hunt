f49d22f6035bf04ab55fe5ecd5419f76acff23b6
Curtis Smith <cj1988333@hotmail.com>
2021-04-11 14:50:01 -0400

    finished validators

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,19 +3,25 @@ const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdBy: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal)
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
-    type: String,
-    default: 'Large'
+        type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+        default: 'Large'
     },
     toppings: [],
     comments: [
